b9de28ec2852508ac0e4fb8322f6e1b61247365c
Saarim Zaman <saarim.z@gmail.com>
2017-09-26 21:29:04 -0400

    added jquery wrapper

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
+$(document).ready(function(){
+    //model object
+    let listData = function(data,time){
+        this.item = ko.observable(data);
+        this.timestamp = ko.observable(time);
+    }
+    //VM
+    let ViewModel = function() {
+        let self = this;
 
-//model object
-let listData = function(data,time){
-    this.item = ko.observable(data);
-    this.timestamp = ko.observable(time);
-}
-//VM
-let ViewModel = function() {
-    let self = this;
+        self.listItems = ko.observableArray([]);
 
-    self.listItems = ko.observableArray([]);
+        self.itemToAdd = ko.observable("");
 
-    self.itemToAdd = ko.observable("");
+        self.addToList = function(){
+            let item = self.itemToAdd();
+            let currentTime = (new Date(Date.now())).toLocaleDateString();
+            self.listItems.push(new listData(item,currentTime));
+            self.itemToAdd("");
+        }
 
-    self.addToList = function(){
-        let item = self.itemToAdd();
-        let currentTime = (new Date(Date.now())).toLocaleDateString();
-        self.listItems.push(new listData(item,currentTime));
-        self.itemToAdd("");
+        self.clearList = function() {
+            self.listItems.removeAll();
+        }
+
+        self.hasItem = ko.computed(function(){
+            return (self.itemToAdd());
+        })
+
+        self.isNotEmpty = ko.computed(function(){
+            return (self.listItems().length);
+        })
+
+        self.removeItem = function(item){
+            self.listItems.remove(item);
+        }
     }
 
-    self.clearList = function() {
-        self.listItems.removeAll();
-    }
-
-    self.hasItem = ko.computed(function(){
-        return (self.itemToAdd());
-    })
-
-    self.isNotEmpty = ko.computed(function(){
-        return (self.listItems().length);
-    })
-
-    self.removeItem = function(item){
-        self.listItems.remove(item);
-    }
-}
-
-//ko apply bindings
-ko.applyBindings(new ViewModel());
-
+    //ko apply bindings
+    ko.applyBindings(new ViewModel());
+});
 
